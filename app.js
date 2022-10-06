e7ddd18b88416b00a387f816021ce56283508457
unknown <ajlagudzevic1100@gmail.com>
2022-10-06 15:42:28 +0200

    Check win

--- app.js
+++ app.js
@@ -9,34 +9,94 @@
 // Pamti score
 // restartuje score
 // pobednik je ko dodje do 5
-
 game = document.getElementById("container");
-const gameMatrix = Array(3).fill(Array(3).fill(0));
-
-const simbolX = "X";
-const simbolO = "O";
-let count = 0;
-
-createTable();
+const gameMatrix = createTable();
+let nextMove = 0;
+let gameOver = false;
+let winSimbol = "";
+let player1 = 0;
+let player2 = 0;
 
 function createTable() {
-  for (let i = 0; i < gameMatrix.length; i++) {
-    for (let j = 0; j < gameMatrix[i].length; j++) {
+  return new Array(3).fill(Array(3).fill(null)).map((arr) =>
+    arr.map(() => {
       const gameDiv = document.createElement("div");
       gameDiv.innerHTML = "";
       gameDiv.className = "game-div";
-      gameMatrix[i][j] = gameDiv;
       gameDiv.addEventListener("click", handleGameDiv);
-      gameDiv.style.backgroundColor = "red";
       game.appendChild(gameDiv);
-    }
-  }
+      return gameDiv;
+    })
+  );
 }
 
 function handleGameDiv(e) {
   if (e.target.innerText !== "") {
     return;
   }
-  count % 2 == 0 ? (e.target.innerText = "X") : (e.target.innerText = "O");
-  count++;
+
+  if (gameOver) {
+    return;
+  }
+  nextMove % 2 == 0 ? (e.target.innerText = "X") : (e.target.innerText = "O");
+  e.target.style.backgroundColor = "blue";
+  nextMove++;
+
+  chechWin();
+}
+
+function chechWin() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameMatrix[i][0].innerText == gameMatrix[i][1].innerText &&
+      gameMatrix[i][1].innerText == gameMatrix[i][2].innerText &&
+      gameMatrix[i][0].innerText !== ""
+    ) {
+      gameMatrix[i][0].id = "win";
+      gameMatrix[i][1].id = "win";
+      gameMatrix[i][2].id = "win";
+      winSimbol = gameMatrix[i][0].innerText;
+      gameOver = true;
+      return;
+    }
+
+    if (
+      gameMatrix[0][i].innerText == gameMatrix[1][i].innerText &&
+      gameMatrix[1][i].innerText == gameMatrix[2][i].innerText &&
+      gameMatrix[0][i].innerText !== ""
+    ) {
+      gameMatrix[0][i].id = "win";
+      gameMatrix[1][i].id = "win";
+      gameMatrix[2][i].id = "win";
+      gameOver = true;
+      winSimbol = gameMatrix[0][i].innerText;
+      return;
+    }
+  }
+
+  if (
+    gameMatrix[0][0].innerText === gameMatrix[1][1].innerText &&
+    gameMatrix[1][1].innerText === gameMatrix[2][2].innerText &&
+    gameMatrix[1][1].innerText !== ""
+  ) {
+    gameMatrix[0][0].id = "win";
+    gameMatrix[1][1].id = "win";
+    gameMatrix[2][2].id = "win";
+    gameOver = true;
+    winSimbol = gameMatrix[0][0].innerText;
+    return;
+  }
+
+  if (
+    gameMatrix[0][2].innerText === gameMatrix[1][1].innerText &&
+    gameMatrix[1][1].innerText === gameMatrix[2][0].innerText &&
+    gameMatrix[0][2].innerText !== ""
+  ) {
+    gameMatrix[0][2].id = "win";
+    gameMatrix[1][1].id = "win";
+    gameMatrix[2][0].id = "win";
+    gameOver = true;
+    winSimbol = gameMatrix[0][2].innerText;
+    return;
+  }
 }
